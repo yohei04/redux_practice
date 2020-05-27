75475fb47e63551a5ab13f100070dd54da79d75c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-27 17:03:33 +0900

    react setting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./actions";
+import Posts from './components/Posts'
+import PostForm from './components/PostForm'
 
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const isLogged = useSelector((state) => state.isLogged);
-  const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <h1>hello {counter}</h1>
-      <button onClick={() => dispatch(increment(5))}>+</button>
-      <button onClick={() => dispatch(decrement())} >-</button>
-
-      {isLogged ? <h3>Valuable Information I shouldn't see</h3> : ""}
+      <PostForm />
+      <hr/>
+      <Posts />
     </div>
   );
 }
