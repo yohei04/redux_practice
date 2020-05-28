967dec437dce10ce13a2dd269621e059db584f7c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-28 10:05:22 +0900

    DE とTMのフォルダを切り分け

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import { Provider } from "react-redux";
-
-import Posts from "./components/Posts";
-import PostForm from "./components/PostForm";
-
-import store from './store'
+// import Posts from "./components/Posts";
+// import PostForm from "./components/PostForm";
+// import store from './store'
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <PostForm />
-        <hr />
-        <Posts />
-      </div>
-    </Provider>
+
   );
 }
 
--- src/TM/reducers/index.js
+++ src/TM/reducers/index.js
@@ -1,12 +1,7 @@
-// import counterReducer from "./counter";
-// import loggedReducer from "./isLogged";
 import { combineReducers } from "redux";
 import postReducer from "./postReducer";
 
 const allReducers = combineReducers({
-  // counter: counterReducer,
-  // isLogged: loggedReducer,
-
   posts: postReducer,
 });
 
