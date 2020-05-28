121c473b147eb56f1507162f9539af9cbac14155
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-28 18:39:42 +0900

    finish DE

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ import { Provider } from "react-redux";
 // import PostForm from "./components/PostForm";
 // import store from './store'
 
+import { useSelector, useDispatch } from "react-redux";
+import {increment, decrement} from './DE/actions'
+
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+      {isLogged ? <h3>Valuable I shouldn't see</h3> : ''}
     </div>
   );
 }
