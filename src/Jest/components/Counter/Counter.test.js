8b4e095ecf456701f2c089126edc19d008a7785b
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-06-09 10:56:33 +0900

    React Testing Library - Testing Redux

---
+++ src/Jest/components/Counter/Counter.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { render, cleanup, fireEvent } from '@testing-library/react';
+import Counter from './Counter';
+
+afterEach(cleanup);
+
+const startingState = { count: 0 };
+const reducer = (state = startingState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(reducer, initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+  };
+};
+
+it('renders with redux', () => {
+  const { getByTestId } = renderWithRedux(<Counter />);
+  expect(getByTestId('count')).toHaveTextContent('0');
+});
+
+it('can increment', () => {
+  const { getByTestId, getByText } = renderWithRedux(<Counter />);
+  fireEvent.click(getByText('+'));
+  expect(getByTestId('count')).toHaveTextContent('1');
+});
+
+it('can decrement', () => {
+  const { getByTestId, getByText } = renderWithRedux(<Counter />);
+  fireEvent.click(getByText('-'));
+  expect(getByTestId('count')).toHaveTextContent('-1');
+});
+
+it('can have initial state', () => {
+  const { getByTestId } = renderWithRedux(<Counter />, {
+    initialState: { count: 5 },
+  });
+  expect(getByTestId('count')).toHaveTextContent('5');
+});
+
+it('can have custom store', () => {
+  const store = createStore(() => ({ count: 35 }));
+  const { getByTestId } = renderWithRedux(<Counter />, { store });
+  expect(getByTestId('count')).toHaveTextContent('35');
+});
